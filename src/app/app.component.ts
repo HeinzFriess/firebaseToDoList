45620df782121ed2f06fab56f5fa003e5534f2c1
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-02-18 19:14:54 +0100

    firebase implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { collection, Firestore, collectionData, setDoc, doc  } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebaseToDoList';
+  toDos$: Observable<any[]>;
+  todoText:string = '';
+  todoName:string = '';
+  
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.toDos$ = collectionData(coll);
+
+    this.toDos$.subscribe(()=> {
+      //hier kann eine Beliebige Funktion aufgerufen werden jedes mal wenn sich ein db Eintrag ändert
+    })
+  }
+
+  addToDo(){
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), {name: this.todoName, text: this.todoText});
+    this.todoName = '';
+    this.todoText = '';
+  }
+  
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-todo-62107',
+    appId: '1:951799743738:web:e3ce53b113f625383d7543',
+    storageBucket: 'fir-todo-62107.appspot.com',
+    apiKey: 'AIzaSyCFXt8K_1ToOp7BRa2DK8woqdi6dLOAJoc',
+    authDomain: 'fir-todo-62107.firebaseapp.com',
+    messagingSenderId: '951799743738',
+  },};
